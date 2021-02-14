76e38b7aa24d6003e44c584df87a84d21cf48110
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-02-14 17:56:12 +0100

    ✨ add multiple container

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,23 +1,2 @@
-/* export type APICAT = {
-  id: number;
-  name: string;
-  image: string;
-  url: string;
-  created: string;
-}; */
-
-/* export type APICats = {
-  info: {
-    count: number;
-    pages: number;
-    next: string;
-    prev: string | null;
-  };
-  results: APICAT[];
-};
-
-export type cat = {
-  imgSrc: string;
-  name: string;
-};
+/* export type APICAT =
  */
