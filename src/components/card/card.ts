742a474b4365c2f438f232f8347178fe605407ac
SilviaBarcellini <barcellinisilvia@gmail.com>
2021-02-14 17:10:02 +0100

    🔥

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,7 +1,7 @@
 import { cat } from "../../utils/api";
 import { createElement } from "../../utils/createElement";
 
-export function createCard({ imgSrc, name, status }) {
+export function createCard({ imgSrc, name, status }: cat) {
   return createElement("article", {
     className: "card",
     children: [
